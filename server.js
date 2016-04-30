960b5e8165e66e46e35d55e6720549b0d8a1e415
furti <daniel.furtlehner@gmx.net>
2016-04-30 15:33:14 +0200

    Added fallback to index.html

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express'),
+  fallback = require('express-history-api-fallback'),
   server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
   server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-  server = express();
+  server = express(),
+  rootFolder = 'target';
 
-server.use(express.static('target'));
+server.use(express.static(rootFolder));
+server.use(fallback('index.html', {
+  root: rootFolder
+}));
 
 server.listen(server_port, server_ip_address, function() {
   console.log("Listening on " + server_ip_address + ", server_port " + server_port);
