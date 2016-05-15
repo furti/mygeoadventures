0c894406e3054b88bb4f02d05e58679d183a2ab9
furti <daniel.furtlehner@gmx.net>
2016-05-15 17:18:32 +0200

    Added dialog with file details

--- src/client/script/FileList.ts
+++ src/client/script/FileList.ts
@@ -1,9 +1,15 @@
 namespace geoadventures {
     class FileList {
+        static $inject = ['$mdDialog'];
+
+        constructor(private $mdDialog: angular.material.IDialogService) {
+
+        }
+
         public icon(file: ProjectFile): string {
             if (file.type === 'file') {
                 if (file.searchable) {
-                  return 'place';
+                    return 'place';
                 }
                 else {
                     return 'description';
@@ -17,6 +23,42 @@ namespace geoadventures {
                     return 'folder_open';
                 }
             }
+        }
+
+        public openFile(file: ProjectFile, $event: MouseEvent): void {
+            var dialog = this.$mdDialog.show({
+                template: `
+<md-dialog>
+  <md-toolbar class="md-accent">
+    <div class="md-toolbar-tools">
+      <h2>{{ $ctrl.file.name }}</h2>
+      <span flex></span>
+      <md-button class="md-icon-button" ng-click="$ctrl.$mdDialog.cancel()">
+        <md-icon aria-label="Close dialog">close</md-icon>
+      </md-button>
+    </div>
+  </md-toolbar>
+  <md-dialog-content class="md-dialog-content">
+      <file-view file="$ctrl.file"></file-view>
+      <p>Content will follow...</p>
+  </md-dialog-content>
+</md-dialog>`,
+                targetEvent: $event,
+                controller: DialogController,
+                bindToController: true,
+                controllerAs: '$ctrl',
+                locals: {
+                    file: file
+                }
+            });
+        }
+    }
+
+    class DialogController {
+        static $inject = ['$mdDialog'];
+
+        constructor(public $mdDialog: angular.material.IDialogService) {
+
         }
     }
 
