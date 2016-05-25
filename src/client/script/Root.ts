ba1459ab278402e8683d45844dedb411c34d5bb9
furti <daniel.furtlehner@gmx.net>
2016-05-25 20:02:40 +0200

    Optimized for mobile devices

--- src/client/script/Root.ts
+++ src/client/script/Root.ts
@@ -1,6 +1,25 @@
 namespace geoadventures {
     class Root {
+        static $inject = ['$mdMedia', '$scope', '$http'];
 
+        public smallDevice: boolean;
+        public loading: boolean;
+        public projects: geoadventures.Project[];
+
+        constructor($mdMedia: angular.material.IMedia, $scope: angular.IScope, $http: angular.IHttpService) {
+            $scope.$watch(function() { return $mdMedia('xs'); }, (small: boolean) => {
+                this.smallDevice = small;
+
+                if (small && !this.projects && !this.loading) {
+                    this.loading = true;
+
+                    $http.get<Project[]>('data/projects').then((response) => {
+                        this.loading = false;
+                        this.projects = response.data;
+                    });
+                }
+            });
+        }
     }
 
     angular.module('geoadventures.root', [])
